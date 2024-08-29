222f723c8a18a104f4348a53f768a4e700c6aaa8
Hamed Fathi <hami.fthi@gmail.com>
2024-08-29 19:13:03 +0200

    Make the entity models simpler

--- entity/blog.go
+++ entity/blog.go
@@ -15,13 +15,12 @@ const (
 
 type Blog struct {
 	gorm.Model
-	Title       string   `gorm:"size:255;not null"`
-	Content     string   `gorm:"type:text;not null"`
-	AuthorID    uint     `gorm:"not null"`
-	Author      User     `gorm:"foreignKey:AuthorID"`
-	CategoryID  uint     `gorm:"not null"`
-	Category    Category `gorm:"foreignKey:CategoryID"`
-	Tags        []Tag    `gorm:"many2many:blog_tags;"`
+	Title       string `gorm:"size:255;not null"`
+	Content     string `gorm:"type:text;not null"`
+	AuthorID    uint   `gorm:"not null"`
+	Author      User   `gorm:"foreignKey:AuthorID"`
+	Category    string `gorm:"size:255"`
+	Tag         string `gorm:"size:255"`
 	PublishedAt *time.Time
 	Status      Status `gorm:"size:50;default:'draft'"`
 }
